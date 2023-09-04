ec746d6cc98569210f518de223ca1c7d8100418f
Brendan Thompson <65555644+DavidBThomp@users.noreply.github.com>
2023-09-04 12:43:25 -0500

    Javascript Basics - Fundamentals Part 1

---
+++ javascript-basics/fundamentals/part1/app.js
@@ -0,0 +1,19 @@
+console.log("Hello from app.js");
+let admin;
+let name;
+name = "john";
+admin = name;
+
+console.log(admin);
+
+console.log(32 + 223);
+
+console.log(21 + 32 + 312 + 5432 + 513 + 531 + 512);
+
+console.log((4 + 6 + 9) / 77);
+
+const MAX = 57;
+
+let actual = MAX - 13;
+
+let percentage = actual / MAX;
