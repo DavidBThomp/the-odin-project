f7d25f56594b29bf3a270cebf8eb1b185acb1f74
Brendan Thompson <65555644+DavidBThomp@users.noreply.github.com>
2023-09-17 17:58:48 -0500

    RPS UI

--- javascript-basics/rock-paper-scissors-project/app.js
+++ javascript-basics/rock-paper-scissors-project/app.js
@@ -1,5 +1,3 @@
-console.log("Hello world");
-
 function getComputerChoice() {
   let computerChoice = Math.random();
   if (computerChoice <= 0.33) {
@@ -11,68 +9,116 @@ function getComputerChoice() {
   }
 }
 
-function getPlayerChoice() {
-  let playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase().trim();
-  if (playerChoice === "rock" || playerChoice === "r") {
-    return "Rock";
-  } else if (playerChoice === "paper" || playerChoice === "p") {
-    return "Paper";
-  } else if (
-    playerChoice === "scissor" ||
-    playerChoice === "s" ||
-    playerChoice === "scissors"
-  ) {
-    return "Scissor";
-  } else {
-    return "Input not recognized.";
-  }
-}
+// function getPlayerChoice() {
+//   let playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase().trim();
+//   if (playerChoice === "rock" || playerChoice === "r") {
+//     return "Rock";
+//   } else if (playerChoice === "paper" || playerChoice === "p") {
+//     return "Paper";
+//   } else if (
+//     playerChoice === "scissor" ||
+//     playerChoice === "s" ||
+//     playerChoice === "scissors"
+//   ) {
+//     return "Scissor";
+//   } else {
+//     return "Input not recognized.";
+//   }
+// }
 
-function playSingleGame(
-  computerChoice = getComputerChoice(),
-  playerChoice = getPlayerChoice()
-) {
-  if (playerChoice === "Input not recognized.") {
-    console.log(playerChoice);
+function playSingleGame(playerPick, computerChoice = getComputerChoice()) {
+  const h1 = document.createElement("h1");
+  const body = document.querySelector("body");
+
+  let h1Check = document.querySelector("h1");
+  if (!h1Check) {
+    body.appendChild(h1);
+  }
+  h1Check = document.querySelector("h1");
+
+  if (playerPick === "Input not recognized.") {
     playSingleGame();
   } else if (
-    (playerChoice === "Rock" && computerChoice === "Scissor") ||
-    (playerChoice === "Paper" && computerChoice === "Rock") ||
-    (playerChoice === "Scissor" && computerChoice === "Paper")
+    (playerPick === "Rock" && computerChoice === "Scissor") ||
+    (playerPick === "Paper" && computerChoice === "Rock") ||
+    (playerPick === "Scissor" && computerChoice === "Paper")
   ) {
-    console.log(`Player Wins! Your ${playerChoice} beats ${computerChoice}.`);
+    h1Check.textContent = `Player Wins! Your ${playerPick} beats ${computerChoice}.`;
     return "playerWin";
-  } else if (playerChoice === computerChoice) {
-    console.log("It's a tie!");
+  } else if (playerPick === computerChoice) {
+    h1Check.textContent = `Both picked ${playerPick}, It's a tie!`;
     return "tie";
   } else {
-    console.log(
-      `Computer Wins, as always. Computers ${computerChoice} beats ${playerChoice}.`
-    );
+    h1Check.textContent = `Computer Wins, as always. Computers ${computerChoice} beats ${playerPick}.`;
     return "computerWin";
   }
 }
 
-function playGame() {
-  let rounds = Number(prompt("How many rounds do you want to play?"));
-  let playerWins = 0;
-  let computerWins = 0;
-  let tie = 0;
-  let i = 0;
+// Global Declare so they keep values
+let playerWins = 0;
+let computerWins = 0;
+let tie = 0;
 
-  while (i < rounds) {
-    i++;
-    let game = playSingleGame();
+function playGame(playerPick) {
+  let game = playSingleGame(playerPick);
 
-    if (game === "playerWin") {
-      playerWins++;
-    } else if (game === "computerWin") {
-      computerWins++;
-    } else if (game === "tie") {
-      tie++;
-    }
-    console.log(`Score: W:${playerWins} L:${computerWins} T:${tie}`);
+  if (game === "playerWin") {
+    playerWins++;
+  } else if (game === "computerWin") {
+    computerWins++;
+  } else if (game === "tie") {
+    tie++;
+  }
+
+  const divCheck = document.querySelector(".result");
+  const body = document.querySelector("body");
+
+  if (!divCheck) {
+    const div = document.createElement("div");
+    div.classList.add("result");
+    body.appendChild(div);
+    const divCheck = document.querySelector(".result");
+    divCheck.textContent = `Score: W:${playerWins} L:${computerWins} T:${tie}`;
+  } else {
+    divCheck.textContent = `Score: W:${playerWins} L:${computerWins} T:${tie}`;
+  }
+
+  if (playerWins > 4 || computerWins > 4) {
+    divCheck.textContent = `Game Over! Score: W:${playerWins} L:${computerWins} T:${tie}`;
+    rock.disabled = true;
+    paper.disabled = true;
+    scissor.disabled = true;
+
+    const restartButton = document.createElement("button");
+    restartButton.textContent = `Restart`;
+    restartButton.classList.add("restart");
+    restartButton.setAttribute("onClick", "restartGame()");
+    body.appendChild(restartButton);
   }
 }
 
-playGame();
+function restartGame() {
+  const restartButton = document.querySelector(".restart");
+  restartButton.remove();
+  rock.disabled = false;
+  paper.disabled = false;
+  scissor.disabled = false;
+
+  const divCheck = document.querySelector(".result");
+  divCheck.textContent = ``;
+
+  playerWins = 0;
+  computerWins = 0;
+  tie = 0;
+
+  let h1Check = document.querySelector("h1");
+  h1Check.textContent = ``;
+}
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissor = document.querySelector("#scissor");
+
+rock.addEventListener("click", () => playGame((userPick = "Rock")));
+paper.addEventListener("click", () => playGame((userPick = "Paper")));
+scissor.addEventListener("click", () => playGame((userPick = "Scissor")));
