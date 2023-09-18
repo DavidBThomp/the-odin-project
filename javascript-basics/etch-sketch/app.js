000f2ca089e97413290c5e90e9f78f4b74f41b77
Brendan Thompson <65555644+DavidBThomp@users.noreply.github.com>
2023-09-18 16:17:12 -0500

    Basic Etch-Sketch Completion

--- javascript-basics/etch-sketch/app.js
+++ javascript-basics/etch-sketch/app.js
@@ -5,26 +5,51 @@ const div = document.createElement("div");
 div.classList.add("sketch-container");
 body.appendChild(div);
 
-// Hover Div
-div.addEventListener("mouseover", (e) => {
-  e.target.style.backgroundColor = "red";
-});
-
 // New Grid Button
 const button = document.querySelector("#new-button");
 button.addEventListener("click", makeGrid);
 
-function makeDivs() {
+function makeDivs(rows = 16) {
   const sketchContainer = document.querySelector(".sketch-container");
-  for (let i = 0; i < 16 * 16; i++) {
+  // Remove Children
+  while (sketchContainer.firstChild) {
+    sketchContainer.removeChild(sketchContainer.firstChild);
+  }
+
+  for (let i = 0; i < rows * rows; i++) {
     const div = document.createElement("div");
     div.classList.add("square");
+    div.style.width = `${600 / rows}px`;
+    div.style.height = `${600 / rows}px`;
     sketchContainer.appendChild(div);
   }
+
+  squareHover();
 }
 
 function makeGrid() {
-  console.log("Connected to Button!");
+  let rows = prompt(`How many rows do you want?`);
+
+  while (isNaN(rows)) {
+    rows = prompt(`Not a valid number. Try again.`);
+  }
+
+  if (rows > 100) {
+    rows = prompt(`Too many rows, max 100!`);
+  }
+
+  makeDivs(rows);
+}
+
+function squareHover() {
+  // Hover Div
+  const square = document.querySelectorAll(".square");
+  square.forEach((square) => {
+    square.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "grey";
+      console.log(`hover`);
+    });
+  });
 }
 
 makeDivs();
